cd371410a46b7c60f36c3ca555e13c12b28250b2
nKeiros13 <76174721+nKeiros13@users.noreply.github.com>
2021-06-12 18:27:15 +0530

    moved async function utility to a separate file

---
+++ utilities/WrapAsync.js
@@ -0,0 +1,8 @@
+//Async function utility
+function wrapAsync(func) {
+    return function (req, res, next) {
+        func(req, res, next).catch(e => next(e));
+    }
+}
+
+module.exports = wrapAsync;
